5ec53f102aadd71b6fdb2fb920e601d82d6b544e
alex.lawrence <alex@alex.>
2024-08-04 21:26:54 +0100

    Fixed error with script

--- install.sh
+++ install.sh
@@ -180,52 +180,52 @@ function add_wincopy() {
 main() {
     init_dir
 
-    gum spin --spinner dot --title "Installing zsh..." -- install_zsh
+    gum spin --spinner dot --title "Installing zsh..." -- bash -c 'install_zsh'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed zsh ✓"
-    gum spin --spinner dot --title "Installing oh-my-zsh..." -- install_oh_my_zsh
+    gum spin --spinner dot --title "Installing oh-my-zsh..." -- bash -c 'install_oh_my_zsh'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed oh-my-zsh ✓"
-    gum spin --spinner dot --title "Installing oh-my-zsh plugins..." -- install_ohmyzsh_plugins
+    gum spin --spinner dot --title "Installing oh-my-zsh plugins..." -- bash -c 'install_ohmyzsh_plugins'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed oh-my-zsh plugins ✓"
-    sudo chmod +x ./zshconfig/p10k/p10k_init.zsh > /dev/null
+    sudo chmod +x ./zshconfig/p10k/p10k_init.zsh
     /bin/zsh ./zshconfig/p10k/p10k_init.zsh
-    gum spin --spinner dot --title "Installing fzf..." -- install_fzf
+    gum spin --spinner dot --title "Installing fzf..." -- bash -c 'install_fzf'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed fzf ✓"
     sudo apt install ripgrep
-    gum spin --spinner dot --title "Installing bat..." -- install_bat
+    gum spin --spinner dot --title "Installing bat..." -- bash -c 'install_bat'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed bat ✓"
-    gum spin --spinner dot --title "Installing eza..." -- install_eza
+    gum spin --spinner dot --title "Installing eza..." -- bash -c 'install_eza'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed eza ✓"
-    gum spin --spinner dot --title "Installing thefuck..." -- install_thefuck
+    gum spin --spinner dot --title "Installing thefuck..." -- bash -c 'install_thefuck'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed thefuck ✓"
-    gum spin --spinner dot --title "Installing zoxide..." -- install_zoxide
+    gum spin --spinner dot --title "Installing zoxide..." -- bash -c 'install_zoxide'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed zoxide ✓"
-    gum spin --spinner dot --title "Installing nvm and node lts..." -- install_node
+    gum spin --spinner dot --title "Installing nvm and node lts..." -- bash -c 'install_node'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed node ✓"
-    gum spin --spinner dot --title "Installing docker..." -- install_docker
+    gum spin --spinner dot --title "Installing docker..." -- bash -c 'install_docker'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed docker ✓"
-    gum spin --spinner dot --title "Installing git-delta..." -- install_git_delta
+    gum spin --spinner dot --title "Installing git-delta..." -- bash -c 'install_git_delta'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed git-delta ✓"
-    gum spin --spinner dot --title "Installing tldr..." -- install_tldr
+    gum spin --spinner dot --title "Installing tldr..." -- bash -c 'install_tldr'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed tldr ✓"
-    gum spin --spinner dot --title "Installing dotnet..." -- install_dotnet
+    gum spin --spinner dot --title "Installing dotnet..." -- bash -c 'install_dotnet'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed dotnet ✓"
-    gum spin --spinner dot --title "Installing glow..." -- install_glow
+    gum spin --spinner dot --title "Installing glow..." -- bash -c 'install_glow'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed glow ✓"
     echo "Are you running on wsl?"
     RUNNING_ON_WSL=$(gum choose "Running on wsl ubuntu" "Standalone ubuntu")
     if [[ "$RUNNING_ON_WSL" == *"wsl"* ]]; then
-        gum spin --spinner dot --title "Removing windows paths, please restart WSL..." -- install_on_wsl 
+        gum spin --spinner dot --title "Removing windows paths, please restart WSL..." -- bash -c 'install_on_wsl '
         gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Removed windows paths ✓"
-        gum spin --spinner dot --title "Adding the wincopy alias to copy file to windows clipboard..." -- add_wincopy
+        gum spin --spinner dot --title "Adding the wincopy alias to copy file to windows clipboard..." -- bash -c 'add_wincopy'
         gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Added wincopy ✓"
     fi
-    gum spin --spinner dot --title "Adding custom aliases..." -- add_custom_aliases
+    gum spin --spinner dot --title "Adding custom aliases..." -- bash -c 'add_custom_aliases'
     gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Added custom aliases ✓"
     echo "Install neovim with custom plugins?"
     INSTALL_NVIM=$(gum choose "Yes" "No")
 
     if [[ "$INSTALL_NVIM" == *"Yes"* ]]; then
-        gum spin --spinner dot --title "Installing neovim..." -- install_neovim
+        gum spin --spinner dot --title "Installing neovim..." -- bash -c 'install_neovim'
         gum style --foreground 34 --border rounded --margin "0" --padding "0 1" "Installed neovim ✓"
     fi
 
